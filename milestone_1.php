7f7c780d6d36b4cd35fba0c36041a5c523d9e3f0
Ronald Luna <ronaldluna1@gmail.com>
2019-01-24 12:18:58 -0800

    pwp

--- milestone_1.php
+++ milestone_1.php
@@ -10,7 +10,7 @@
 		<lu>
 			<li id="item1">Age: 31</li>
 			<li id="item2">Profession: Student at CNM part time. Studying to be an Accountant</li>
-			<li id="item3">Technology: IPhoneX 256 GB Microsoft Surface Pro 12.3″ - Core i5 7300U 2.6 GHz - 8 GB RAM - 256 GB SSD
+			<li id="item3">Technology: IPhoneX 256 GB, Microsoft Surface Pro 12.3″ - Core i5 7300U 2.6 GHz - 8 GB RAM - 256 GB SSD,
 				Dell Inspiron Desktop (Intel ) w/ Intel Core - 8GB - 16GB - Windows 10 Home</li>
 		</lu>
 		<h1>Attitudes and Behaviors:</h1>
