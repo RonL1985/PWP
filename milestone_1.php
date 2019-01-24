efaa93b01df90901be431b61b0def14bb153cae7
Ronald Luna <ronaldluna1@gmail.com>
2019-01-24 12:36:04 -0800

    pwp

--- milestone_1.php
+++ milestone_1.php
@@ -28,26 +28,26 @@
 	</head>
 	<body>
 		<img src="images/personapic.jpeg">
-		<h1>Name: Nichole Barr</h1>
+		<h1 style="color:greenyellow;">Name: Nichole Barr</h1>
 		<lu>
-			<li id="item1">Age: 31</li>
-			<li id="item2">Profession: Student at CNM part time. Studying to be an Accountant</li>
-			<li id="item3">Technology: IPhoneX 256 GB, Microsoft Surface Pro 12.3″ - Core i5 7300U 2.6 GHz - 8 GB RAM - 256 GB SSD,
+			<li style="color:greenyellow;" id="item1">Age: 31</li>
+			<li style="color:greenyellow;" id="item2">Profession: Student at CNM part time. Studying to be an Accountant</li>
+			<li style="color:greenyellow;" id="item3">Technology: IPhoneX 256 GB, Microsoft Surface Pro 12.3″ - Core i5 7300U 2.6 GHz - 8 GB RAM - 256 GB SSD,
 				Dell Inspiron Desktop (Intel ) w/ Intel Core - 8GB - 16GB - Windows 10 Home</li>
 		</lu>
-		<h1>Attitudes and Behaviors:</h1>
-		<p>Strong mother of 3 children that studies all the time to accomplish her college education.
+		<h1 style="color:greenyellow;">Attitudes and Behaviors:</h1>
+		<p style="color:greenyellow;">Strong mother of 3 children that studies all the time to accomplish her college education.
 		she is calm and does not blow up over the little things she just keeps moving forward even in hard times.
 		she has a real positive attitude.</p>
-		<h1>Frustrations and needs:</h1>
-		<p>Not being able to find the right dog for the family. Nichole is looking for a fun and playful dog that is
+		<h1 style="color:greenyellow;">Frustrations and needs:</h1>
+		<p style="color:greenyellow;">Not being able to find the right dog for the family. Nichole is looking for a fun and playful dog that is
 			good around kids and listens when it is told to do or not to do thing and is 100% potty trained.</p>
-		<h1>Goals:</h1>
-		<p>Nichole is hoping to have a quick and easy way of finding the right dog for her and her family by trying this new online
+		<h1 style="color:greenyellow;">Goals:</h1>
+		<p style="color:greenyellow;">Nichole is hoping to have a quick and easy way of finding the right dog for her and her family by trying this new online
 			pet adoption experience she is hoping to make her kids happy with a new member  of the family. Her kids have biting at her heals
 		about getting a new dog.</p>
-		<h1>User Story:</h1>
-		<p>Nichole is a single mom of three beautiful kids that are her world. Her kids have been asking and begging her to get a new dog for them.
+		<h1 style="color:greenyellow;">User Story:</h1>
+		<p style="color:greenyellow;">Nichole is a single mom of three beautiful kids that are her world. Her kids have been asking and begging her to get a new dog for them.
 		So she has been looking all over town at the shelters and has no luck finding the right one for her kids. She is looking forward
 		to trying the new online pet adoption sites to see if the right dog is waiting for her to bring home.</p>
 
