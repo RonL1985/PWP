1314100b69c2703681e621c5e31337a18bab35c8
Ronald Luna <ronaldluna1@gmail.com>
2019-01-23 20:27:12 -0800

    pwp

--- milestone_1.php
+++ milestone_1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title>persona</title>
@@ -13,6 +13,13 @@
 			<li id="item3">IPhoneX 256 GB Microsoft Surface Pro 12.3″ - Core i5 7300U 2.6 GHz - 8 GB RAM - 256 GB SSD
 				Dell Inspiron Desktop (Intel ) w/ Intel Core - 8GB - 16GB - Windows 10 Home</li>
 		</lu>
+		<h1>Attitudes and Behaviors:</h1>
+		<p>Strong mother of 3 children that studies all the time to accomplish her college education.
+		she is calm and does not blow up over the little things she just keeps moving forward even in hard times.
+		she has a real positive attitude.</p>
+		<h1>Frustrations and needs:</h1>
+		<p>Not being able to find the right dog for the family. Nichole is looking for a fun and playful dog that is
+			good around kids and listens when it is told to do or not to do thing and is 100% potty trained.</p>
 
 	</body>
 </html>
