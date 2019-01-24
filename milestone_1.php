9977e78090fbf304c583d90b76f48950b07d2001
Ronald Luna <ronaldluna1@gmail.com>
2019-01-23 19:42:34 -0800

    pwp

--- milestone_1.php
+++ milestone_1.php
@@ -13,6 +13,9 @@
 		<img src="personapic.jpeg">
 		<p>Situation: Nichole has been looking to have a new dog.She has been all over the place looking to get a new dog but has no success.
 		So a friend of her's recommended that she should look online in hopes of finding a good dog to adopt.</p>
-
+		<p>Frustrations and Needs: Not being able to find the right dog for the family. Nichole is looking for a fun and playful dog that is
+		good around kids and listens and is 100% potty trained.</p>
+		<p>Goals: Nichole is hoping to have a quick and easy way of finding the right dog for you and her family by trying this new online
+		pet adoption experience</p>
 	</body>
 </html>
