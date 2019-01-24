5779c9700071fbd32c25c4060f27e8dd5d928be1
Ronald Luna <ronaldluna1@gmail.com>
2019-01-23 18:59:48 -0800

    pwp

---
+++ milestone_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html> lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="index.stylesheet.css" type="text/css"/>
+		<title>persona</title>
+	</head>
+	<body class="body">
+		<h1>Name: Nichole Barr</h1>
+		<p>Age: 31</p>
+		<p>Student at CNM part time</p>
+		<p>Technology: IPhone7 Plus with IOS system</p>
+		<p>Behaviors and Attitudes: Responsible mother of 3 with a positive attitude</p>
+		<img src="personapic.jpeg">
+		<p>Situation: Nichole has been looking to have a new dog.She has been all over the place looking to get a new dog but has no success.
+		So a friend of her's recommended that she should look online in hopes of finding a good dog to adopt.</p>
+
+	</body>
+</html>
